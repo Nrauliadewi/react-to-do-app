178fa6dc84c30a5751316f3780390825987b02e7
Nrauliadewi <159509188+Nrauliadewi@users.noreply.github.com>
2024-06-09 21:58:12 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,64 @@
-import ".App.css";
+import { useState } from "react";
+//import "./App.css";
+import Todos from "./components/Todos";
+
+const DUMMY = [
+  {
+    id: 1,
+    title: "Finish Progate React Course",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Have lunch with Guru Domba",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Study React with Ninja Ken",
+    completed: false,
+  },
+];
 
 function App() {
-  return <h1>hello React</h1>;
+  const [todos, setTodos] = useState(DUMMY);
+
+  const toggleCompleted = (id) => {
+    const updateTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+
+    setTodos(updateTodos);
+  };
+
+  const deleteTodo = (id) => {
+    const filterDeleted = todos.filter((todo) => {
+      return todo.id !== id;
+    });
+
+    setTodos(filterDeleted);
+  };
+
+  return (
+    <div style={styles.container}>
+      <h1>My Todo List</h1>
+      <Todos
+        todos={todos}
+        toggleCompleted={toggleCompleted}
+        deleteTodo={deleteTodo}
+      />
+    </div>
+  );
 }
+
+const styles = {
+  container: {
+    textAlign: "center",
+    padding: "12px",
+  },
+};
 
 export default App;
